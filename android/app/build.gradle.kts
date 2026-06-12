dd93f87815f8c6a03474e4167d9250598a316110
PRANTA Dutta <prantadutta1997@gmail.com>
2026-06-12 21:29:11 +0600

    Add core dependencies and set Android package to com.pranta.deadbounce

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.deadbounce_flutter_app"
+    namespace = "com.pranta.deadbounce"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
